35e718ed3726c9fb61efa11b0232d42af939f491
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2016-12-12 08:23:24 +0700

    menambahkan file baru

---
+++ posts/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Post(models.Model):
+	title = models.CharField(max_length=120)
+	content = models.TextField()
+	update = models.DateTimeField(auto_now=True, auto_now_add=False)
+	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+	# for python 3
+	def __unicode__(self):
+		return self.title
+
+	# for python 2
+	def __str__(self):
+		return self.title
