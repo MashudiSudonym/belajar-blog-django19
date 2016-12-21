709ff54247b7e23d52d9e948f69c381af9bc9a1c
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2016-12-21 22:24:50 +0700

    belajar banyak tentang cara mengambil data dari tabel Posts

--- posts/views.py
+++ posts/views.py
@@ -1,16 +1,31 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post
 
 # Create your views here.
 
 def post_create(request):
 	return HttpResponse("<h1>Create</h1>")
 
-def post_detail(request):
-	return HttpResponse("<h1>Detail</h1>")
+def post_detail(request, id=None):
+	instance = get_object_or_404(Post, id=id)
+	context = {
+		"title": "Detail",
+		"instance": instance,
+	}
+
+	return render(request, "post_detail.html", context)
 
 def post_list(request):
-	return HttpResponse("<h1>List</h1>")
+	queryset = Post.objects.all()
+	context = {
+		"object_list": queryset,
+		"title": "List",
+
+	}
+
+	return render(request, "index.html", context)
 
 def post_update(request):
 	return HttpResponse("<h1>Update</h1>")
